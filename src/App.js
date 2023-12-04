afa2105f4837168155c3055b7e1969ac11bca38f
Vivekkashyap043 <vivekkashyap043@gmail.com>
2023-12-04 09:08:00 +0530

    up to assignment 9

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import './App.css';
-import logo from './logo.svg';
-import './App.css';
-import Array_Iteration from './components/assignment-1/Array_Iteration';
-import Root from './components/assignment-2/Root';
-import MyProducts from './components/assignment-3/MyProducts';
-import Navbar from './components/assignment-4/users/Navbar';
-import Footer from './components/assignment-4/users/Footer';
-import UserList from './components/assignment-4/users/UserList'
-import Parent from './components/assignment-4/parent-child/Parent';
-import Users from './components/assignment-5/Users';
-import AddUser from './components/assignment-7/AddUser';
-import Users from './components/assignment-5/Users';
-import RegistrationForm from './components/assignment-6/RegistrationForm';
-import AddUser from './components/assignment-7/AddUser';
-import Todo from './components/assignment-8/Todo'
+import Router from './components/assignment-9/Router';
 
 function App() {
 
   return (
     <div>
-      <RegistrationForm />
+      <Router />
     </div>
   );
 }
